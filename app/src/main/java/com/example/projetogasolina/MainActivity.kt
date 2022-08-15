836c5ffd5169fc6fa2d658ba1191b2d4fbad0e08
Marinaldo Ferreira <marinaldofsilva@gmail.com>
2022-08-14 21:55:43 -0300

    projeto calcularo imc

--- app/src/main/java/com/example/projetogasolina/MainActivity.kt
+++ app/src/main/java/com/example/projetogasolina/MainActivity.kt
@@ -1,58 +1,79 @@
 package com.example.projetogasolina
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-//teste
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        calcular_btn.setOnClickListener {
 
-    fun calcularPreco(view: View) {
-        val precoAlcool = editPrecoAlcool.text.toString()
-        val precoGasolina = editPrecoGasolina.text.toString()
+            // verificando se altura e peso não estão vazio
+            if (eAltura.text.isNotEmpty() && ePeso.text.isNotEmpty()) {
+                val altura = (eAltura.text.toString()).toInt()
+                val peso = (ePeso.text.toString()).toInt()
 
-        val validaCampos = validaCampos(precoAlcool, precoGasolina)
-        if (validaCampos) {
-            calcularMelhorPreco(precoAlcool, precoGasolina)
+                // calcular o imc
+                val IMC = calculateBMI(altura, peso)
 
-        } else {
-            textResultado.text = "Prencha os preços primeiros"
+                imc.text = IMC.toString()
+                imc.visibility = View.VISIBLE
+
+                // condição para calculo do imc, depenendo da altura e peso vai ser exibido uma mesangem de acordo
+                if (IMC < 18.5) {
+                    status.text = "Abaixo do peso"
+                } else if (IMC >= 18.5 && IMC < 24.9) {
+                    status.text = "Está Saudável"
+                } else if (IMC >= 24.9 && IMC < 30) {
+                    status.text = "Acima do Peso"
+                } else if (IMC >= 30) {
+                    status.text = "Sofrendo de Obesidade"
+                }
+
+                bmi_tv.visibility = View.VISIBLE
+                status.visibility = View.VISIBLE
+
+                ReCalcular.visibility = View.VISIBLE
+                calcular_btn.visibility = View.GONE
+
+            }
+
+            //quando altura e peso for nulo vai ser exibido essa frase
+            else {
+                Toast.makeText(this, "Insira a altura e o peso válidos", Toast.LENGTH_SHORT).show()
+            }
         }
 
+        ReCalcular.setOnClickListener {
+            ResetEverything()
+        }
 
     }
 
-    fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
-        val valorAlcool = precoAlcool.toDouble()
-        val valorGasolina = precoGasolina.toDouble()
+    // Função para redefinir todos os campos Text e EditText.
+    private fun ResetEverything() {
 
-        val resultadoPreco = valorAlcool / valorGasolina
+        calcular_btn.visibility = View.VISIBLE
+        ReCalcular.visibility = View.GONE
 
-        if (resultadoPreco >= 0.7){
-            textResultado.text = "Melhor ultilizar Gasolina"
-
-        }else{
-            textResultado.text = "Melhor ultilizar Alcool"
-        }
-
-
+        eAltura.text.clear()
+        ePeso.text.clear()
+        status.text = " "
+        imc.text = " "
+        bmi_tv.visibility = View.GONE
     }
 
+    // Função para cálculo do IMC
+    private fun calculateBMI(altura: Int, peso: Int): Float {
 
-    fun validaCampos(precoAlcool: String, precoGasolina: String) : Boolean {
-        var camposValidados: Boolean = true
-        if(precoAlcool == null || precoAlcool.equals("")){
-            camposValidados = false
-        } else if (precoGasolina == null || precoGasolina.equals("")){
-            camposValidados = false
-        }
-        return camposValidados
+        val Altura_metro = altura.toFloat() / 100
+        val IMC = peso.toFloat() / (Altura_metro * Altura_metro)
 
+        return IMC
     }
 }

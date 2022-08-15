04549c2655f1dc1c0bae61cb1a89bf6f21f01208
Marinaldo Ferreira <marinaldofsilva@gmail.com>
2022-08-14 22:05:39 -0300

    adicionando comentarios

--- app/src/main/java/com/example/projetogasolina/MainActivity.kt
+++ app/src/main/java/com/example/projetogasolina/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-
+    //função que calcula o valor dos combustiveis
     fun calcularPreco(view: View) {
         val precoAlcool = editPrecoAlcool.text.toString()
         val precoGasolina = editPrecoGasolina.text.toString()
@@ -29,9 +29,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+        //passar os valores para double
         val valorAlcool = precoAlcool.toDouble()
         val valorGasolina = precoGasolina.toDouble()
-
+        //divide os valores de gasolina e alcool
         val resultadoPreco = valorAlcool / valorGasolina
 
         if (resultadoPreco >= 0.7){
@@ -47,6 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     fun validaCampos(precoAlcool: String, precoGasolina: String) : Boolean {
         var camposValidados: Boolean = true
+        //verifica se os campos não estão nulo
         if(precoAlcool == null || precoAlcool.equals("")){
             camposValidados = false
         } else if (precoGasolina == null || precoGasolina.equals("")){
